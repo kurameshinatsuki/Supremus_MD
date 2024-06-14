6c9ad929dcea569a0587e1d2c46975e2dd2ed1c1
johnsupremus1 <122124870+johnsupremus1@users.noreply.github.com>
2024-06-14 23:42:19 +0000

    Create yugiohdeck.js

---
+++ commandes/yugiohdeck.js
@@ -0,0 +1,591 @@
+const { zokou } = require('../framework/zokou');
+
+zokou(
+    {
+        nomCom: 'deckyugi',
+        categorie: 'rpss'
+    },
+    async (dest, zk, commandeOptions) => {
+        const { repondre, arg, ms } = commandeOptions;
+
+        if (!arg || arg.length === 0)  {
+            const lien = 'https://telegra.ph/file/88ca5ed1705cc665f0635.jpg';
+            const msg = `░░░░░░░░░░░░░░░░░░░░░
+═════════════════════
+       *🀄 DECK PRINCIPAL 🀄*
+═════════════════════
+
+- Magicien du Chaos Sombre
+- Soldat du Lustre Noir
+- Gaïa le Chevalier Implacable
+- Magicien Sombre
+- Crâne Invoqué
+- Kaiser Insolent
+- Malédiction du Dragon
+- Tortue Catapulte
+- Dragon Ailé, Gardien de la Forteresse N°1
+- Dragon Koumori
+- Elfe Mystique
+- Gardien Celte
+- Cimetiére de Mammouth
+- Crâne Invoqué
+- Soldat Géant de Pierre
+- Kuriboh
+- Lutin Sauvage
+- Guerrier Castor
+- Griffore
+- Croc Argenté
+- Épée de Révélation de la Lumière
+- Flèche Brise-Sort
+- Libération d'Âme
+- Livre d'Arts Secrets
+- Multiplicateur
+- Cercle Envoûtant
+- Chapeaux Magiques
+- Force de Miroir
+- L'œil de la Vérité
+- Transfert
+- Rituel du Lustre Noir
+
+═════════════════════
+          *🎴 EXTRA DECK 🎴*
+═════════════════════
+
+- Gaia le Dragon Champion
+
+═════════════════════
+░░░░░░░░░░░░░░░░░░░░░`;
+            zk.sendMessage(dest, { image: { url: lien }, caption: msg }, { quoted: ms });
+   
+        }
+    }
+);
+
+zokou(
+    {
+        nomCom: 'yugilink',
+        categorie: 'rpss'
+    },
+    async (dest, zk, commandeOptions) => {
+        const { repondre, arg, ms } = commandeOptions;
+
+        if (!arg || arg.length === 0)  {
+            const lien = 'https://telegra.ph/file/88ca5ed1705cc665f0635.jpg';
+            const msg = `░░░░░░░░░░░░░░░░░░░░░
+═════════════════════
+        *👤 COMPÉTENCES 👤*
+═════════════════════
+
+- *Pioche de la Destinée*
+ 『https://telegra.ph/file/e67a124967e15ad0959a0.jpg』
+
+- *Pioche Final*
+ 『https://telegra.ph/file/eca053745d516a0f11147.jpg』
+
+═════════════════════
+       *🀄 DECK PRINCIPAL 🀄*
+═════════════════════
+
+- *Magicien du Chaos Sombre*
+ 『https://telegra.ph/file/d6ce5d74b57ef38dca203.jpg』
+
+- *Soldat du Lustre Noir*
+ 『https://telegra.ph/file/77d6d5a2d5c94b23d9411.jpg』
+
+- *Gaïa le Chevalier Implacable*
+ 『https://telegra.ph/file/859d6f0716108cacbf98c.jpg』
+
+- *Magicien Sombre*
+ 『https://telegra.ph/file/9973732bd8ee761ba4faa.jpg』
+
+- *Crâne Invoqué ×2*
+ 『https://telegra.ph/file/5179cdd76c579fa487ff9.jpg』
+
+- *Kaiser Insolent*
+ 『https://telegra.ph/file/ae20c3baff1cc94571ac3.jpg』
+
+- *Malédiction du Dragon*
+ 『https://telegra.ph/file/afa6a7e784dc4ab36d87d.jpg』
+
+- *Tortue Catapulte*
+ 『https://telegra.ph/file/e0fd883c12ec658ff8576.jpg』
+
+- *Dragon Ailé, Gardien de la Forteresse N°1*
+ 『https://telegra.ph/file/40e76694d72feae23c105.jpg』
+
+- *Dragon Koumori*
+ 『https://telegra.ph/file/6dc335fa40747459619b5.jpg』
+
+- *Elfe Mystique*
+ 『https://telegra.ph/file/6e500e5f00336d71c896e.jpg』
+
+- *Gardien Celte*
+ 『https://telegra.ph/file/4e05524ebdb8459ca4042.jpg』
+
+- *Cimetiére de Mammouth*
+ 『https://telegra.ph/file/ad11d78b6bc195f021218.jpg』
+
+- *Soldat Géant de Pierre*
+ 『https://telegra.ph/file/c8b4f88634b354244253e.jpg』
+
+- *Kuriboh*
+ 『https://telegra.ph/file/a3ff47ea464257753223f.jpg』
+
+- *Lutin Sauvage*
+ 『https://telegra.ph/file/13f19915761924fd7b9ee.jpg』
+
+- *Guerrier Castor*
+ 『https://telegra.ph/file/55d777d21cb91e1784245.jpg』
+
+- *Griffore*
+ 『https://telegra.ph/file/96cd570c7610322a1abac.jpg』
+
+- *Croc Argenté*
+ 『https://telegra.ph/file/1be0412389ab8fdf3b3f4.jpg』
+
+- *Épée de Révélation de la Lumière*
+ 『https://telegra.ph/file/99a7039b9f2b4b2fbd871.jpg』
+
+- *Flèche Brise-Sort*
+ 『https://telegra.ph/file/2a4b6ca6108f13f070289.jpg』
+
+- *Libération d'Âme*
+ 『https://telegra.ph/file/04be931f0645a9658c981.jpg』
+
+- *Livre d'Arts Secrets*
+ 『https://telegra.ph/file/24c48305e57060c89bc2e.jpg』
+
+- *Multiplicateur*
+ 『https://telegra.ph/file/e5d483b66dccaaf13566d.jpg』
+
+- *Cercle Envoûtant*
+ 『https://telegra.ph/file/7ca6fd9ee22a0101f0cfe.jpg』
+
+- *Chapeaux Magiques*
+ 『https://telegra.ph/file/03d5ba3df3dedb903b5a1.jpg』
+
+- *Force de Miroir*
+ 『https://telegra.ph/file/58f2f669fee4f7310cb60.jpg』
+
+- *L'œil de la Vérité*
+ 『https://telegra.ph/file/bc8326dbd0edec31e57a8.jpg』
+
+- *Transfert*
+ 『https://telegra.ph/file/9aa2c190813fc689845ef.jpg』
+
+- *Rituel du Lustre Noir*
+ 『https://telegra.ph/file/2b307544b8fa310171f5c.jpg』
+
+═════════════════════
+          *🎴 EXTRA DECK 🎴*
+═════════════════════
+
+- Gaia le Dragon Champion
+ 『https://telegra.ph/file/689c884be6691795fdcf7.jpg』
+
+═════════════════════
+░░░░░░░░░░░░░░░░░░░░░`;
+            zk.sendMessage(dest, { image: { url: lien }, caption: msg }, { quoted: ms });
+   
+        }
+    }
+);
+
+zokou(
+    {
+        nomCom: 'deckkaiba',
+        categorie: 'rpss'
+    },
+    async (dest, zk, commandeOptions) => {
+        const { repondre, arg, ms } = commandeOptions;
+
+        if (!arg || arg.length === 0)  {
+            const lien = 'https://telegra.ph/file/0a144738c0084d3e02a6a.jpg';
+            const msg = `░░░░░░░░░░░░░░░░░░░░░
+═════════════════════
+       *🀄 DECK PRINCIPAL 🀄*
+═════════════════════
+
+- Obelisk, le Tourmenteur
+- Dragon Blanc aux Yeux Bleus
+- Dragon Blanc aux Yeux Bleus
+- Dragon Blanc aux Yeux Bleus
+- Dragon Étincelant N°2
+- Planeur du Kaiser
+- Dragon Extra-Dimensionnel
+- Seigneur Vampire
+- Dragon Étincelant
+- Malédiction du Dragon
+- La Jinn le Génie Mystique de la Lampe
+- Lance Dragon
+- Maraudeur Vorse
+- Seigneur des D
+- Tank De Métal Z
+- Tête de Canon X
+- Tête de Dragon Y
+- Kaibaman
+- Peten le Clown des Ténèbres
+- CyberStein
+- Anciens Règlements
+- Bourse des Ames
+- Mégamorphe
+- Flot Rugissant de Destruction
+- Polymérization
+- Monster Reborn
+- Appel de l'Être Hanté
+- Chaîne Démoniaque
+- Virus Démoniaque de Destruction de Deck
+- Sortilège de l'Ombre
+
+═════════════════════
+          *🎴 EXTRA DECK 🎴*
+═════════════════════
+
+- Dragon Ultime aux Yeux Bleus
+- Canon Dragon XYZ
+- Canon Dragon XY
+- Canon Tank XZ
+- Dragon Tank YZ
+
+═════════════════════
+░░░░░░░░░░░░░░░░░░░░░`;
+            zk.sendMessage(dest, { image: { url: lien }, caption: msg }, { quoted: ms });
+   
+        }
+    }
+);
+
+zokou(
+    {
+        nomCom: 'kaibalink',
+        categorie: 'rpss'
+    },
+    async (dest, zk, commandeOptions) => {
+        const { repondre, arg, ms } = commandeOptions;
+
+        if (!arg || arg.length === 0)  {
+            const lien = 'https://telegra.ph/file/0a144738c0084d3e02a6a.jpg';
+            const msg = `░░░░░░░░░░░░░░░░░░░░░
+═════════════════════
+        *👤 COMPÉTENCES 👤*
+═════════════════════
+
+- *Raclée*
+ 『https://telegra.ph/file/cccecd9a8101848fa137c.jpg』
+
+- *Infection Virale*
+ 『https://telegra.ph/file/41eccbf68a1be8de1b902.jpg』
+
+═════════════════════
+       *🀄 DECK PRINCIPAL 🀄*
+═════════════════════
+
+- *Obelisk, le Tourmenteur*
+ 『https://telegra.ph/file/210178bbb4cb8166ea9b1.jpg』
+
+- *Dragon Blanc aux Yeux Bleus ×3*
+ 『https://telegra.ph/file/4fc61987d1a79c77cb5cd.jpg』
+
+- *Dragon Étincelant N°2*
+ 『https://telegra.ph/file/63986e8a3e2afefff54d7.jpg』
+
+- *Planeur du Kaiser*
+ 『https://telegra.ph/file/4b7381e4461b731184c41.jpg』
+
+- *Dragon Extra-Dimensionnel*
+ 『https://telegra.ph/file/c14bbd4ba8ddb8fde4cc4.jpg』
+
+- *Seigneur Vampire*
+ 『https://telegra.ph/file/4841535df72b590e870ec.jpg』
+
+- *Dragon Étincelant*
+ 『https://telegra.ph/file/c59ed71749b22cf847633.jpg』
+
+- *Kaiser Hyppocampe*
+ 『https://telegra.ph/file/a52adf012d1fba5ccddc3.jpg』
+
+- *La Jinn le Génie Mystique de la Lampe*
+ 『https://telegra.ph/file/eb2d42fbc34dad339fad5.jpg』
+
+- *Lance Dragon*
+ 『https://telegra.ph/file/b506d8ed93942e22c2f05.jpg』
+
+- *Maraudeur Vorse*
+ 『https://telegra.ph/file/6018e26ce1f07e81423d9.jpg』
+
+- *Seigneur des D*
+ 『https://telegra.ph/file/1c7c781e25d17beba5cbb.jpg』
+
+- *Tank De Métal Z*
+ 『https://telegra.ph/file/7527d98bc6ff8b3b49e1b.jpg』
+
+- *Tête de Canon X*
+ 『https://telegra.ph/file/1623ac3a5ea1779642dd3.jpg』
+
+- *Tête de Dragon Y*
+ 『https://telegra.ph/file/b5da4690278110cc0efb7.jpg』
+
+- *Kaibaman*
+ 『https://telegra.ph/file/235453cd0d0d873f9e5dd.jpg』
+
+- *Peten le Clown des Ténèbres*
+ 『https://telegra.ph/file/4d4371e954c5b40639fc6.jpg』
+
+- *CyberStein*
+ 『https://telegra.ph/file/ada9c91526fc55d3a78a0.jpg』
+
+- *Anciens Règlements*
+ 『https://telegra.ph/file/6e9b17851b0b2c7889e8e.jpg』
+
+- *Bourse des Ames*
+ 『https://telegra.ph/file/5a7d5ec5981071c0ef129.jpg』
+
+- *Mégamorphe*
+ 『https://telegra.ph/file/5a714cc839f2ef6c758e9.jpg』
+
+- *Flot Rugissant de Destruction*
+ 『https://telegra.ph/file/d68042259e49920baca1e.jpg』
+
+- *Polymérization*
+ 『https://telegra.ph/file/1f7b8a6ec55986a3d0a1a.jpg』
+
+- *Monster Reborn*
+ 『https://telegra.ph/file/3cc55ecd3f993c09f62d4.jpg』
+
+- *Appel de l'Être Hanté*
+ 『https://telegra.ph/file/ca07504762607d6406114.jpg』
+
+- *Chaîne Démoniaque*
+ 『https://telegra.ph/file/44559014d5c155c5adfae.jpg』
+
+- *Virus Démoniaque de Destruction de Deck*
+ 『https://telegra.ph/file/766ac8cf43b905f0b7355.jpg』
+
+- *Sortilège de l'Ombre*
+ 『https://telegra.ph/file/2eab305c79ac208917b9c.jpg』
+
+═════════════════════
+          *🎴 EXTRA DECK 🎴*
+═════════════════════
+
+- *Dragon Ultime aux Yeux Bleus*
+ 『https://telegra.ph/file/316131cf79a8fa208afed.jpg』
+
+- *Canon Dragon XYZ*
+ 『https://telegra.ph/file/d21c113ab50fcbacf5af1.jpg』
+
+- *Canon Dragon XY*
+ 『https://telegra.ph/file/620a2a6c83f3f1bdf5f0d.jpg』
+
+- *Canon Tank XZ*
+ 『https://telegra.ph/file/01bfd68e88876677bcb59.jpg』
+
+- *Dragon Tank YZ*
+ 『https://telegra.ph/file/d15e9ff45b1a507c7d890.jpg』
+
+═════════════════════
+░░░░░░░░░░░░░░░░░░░░░`;
+            zk.sendMessage(dest, { image: { url: lien }, caption: msg }, { quoted: ms });
+   
+        }
+    }
+);
+
+zokou(
+    {
+        nomCom: 'deckjoey',
+        categorie: 'rpss'
+    },
+    async (dest, zk, commandeOptions) => {
+        const { repondre, arg, ms } = commandeOptions;
+
+        if (!arg || arg.length === 0)  {
+            const lien = 'https://telegra.ph/file/8eaccbe6b5fd91e8f7378.jpg';
+            const msg = `░░░░░░░░░░░░░░░░░░░░░
+═════════════════════
+       *🀄 DECK PRINCIPAL 🀄*
+═════════════════════
+
+- Gilford, l'Éclair
+- Dragon Noir aux Yeux Rouges
+- Dragon Noir aux Yeux Rouges
+- Dragon Météore
+- Crâne Invoqué
+- Gearfried le Chevalier de Fer
+- Axe Raider
+- Guerrier Panthère
+- Guerrier Roquette
+- Masaki le Spadassin Légendaire
+- Petit Guerrier Allié
+- Spadassin des Flammes Bleues
+- Troupe d'Assaut Gobeline
+- Bébé Dragon
+- Bébé Dragon
+- Glaive de l'Alligator
+- Compagnon du Spadassin De Landstar
+- Le Spadassin de Landstar
+- Manipulateur de Flammes
+- Magicien du Temps
+- Copiechat
+- Bouclier et Épée
+- Le Guerrier Réincarné
+- Renfort de l'Armée
+- Bouclier Magique de Bras
+- Kunai avec Chaîne
+- Profanateur de Tombes
+- Trappe sans Fond
+- Dé-Crâne
+
+═════════════════════
+          *🎴 EXTRA DECK 🎴*
+═════════════════════
+
+- Dragon Crâne Noir
+- Dragon Noir Météore
+- Dragon Millénaire
+- Dragon du Glaive de l'Alligator
+- Spadassin des Flammes
+
+═════════════════════
+░░░░░░░░░░░░░░░░░░░░░`;
+            zk.sendMessage(dest, { image: { url: lien }, caption: msg }, { quoted: ms });
+   
+        }
+    }
+);
+
+zokou(
+    {
+        nomCom: 'joeylink',
+        categorie: 'rpss'
+    },
+    async (dest, zk, commandeOptions) => {
+        const { repondre, arg, ms } = commandeOptions;
+
+        if (!arg || arg.length === 0)  {
+            const lien = 'https://telegra.ph/file/8eaccbe6b5fd91e8f7378.jpg';
+            const msg = `░░░░░░░░░░░░░░░░░░░░░
+═════════════════════
+        *👤 COMPÉTENCES 👤*
+═════════════════════
+
+- *Dernier Pari*
+ 『https://telegra.ph/file/9c1027d5d93b587e0e3dd.jpg』
+
+═════════════════════
+       *🀄 DECK PRINCIPAL 🀄*
+═════════════════════
+
+- *Gilford, l'Éclair*
+ 『https://telegra.ph/file/c900d11a59605f841ff74.jpg』
+
+- *Dragon Noir aux Yeux Rouges 2×*
+ 『https://telegra.ph/file/3ba24e0e003bf82bcf168.jpg』
+
+- *Crâne Invoqué*
+ 『https://telegra.ph/file/5179cdd76c579fa487ff9.jpg』
+
+- *Dragon Météore*
+ 『https://telegra.ph/file/caa411a36c0e1ab0a2df9.jpg』
+
+- *Gearfried le Chevalier de Fer*
+ 『https://telegra.ph/file/8bbcc944bcfa91c0f4fd7.jpg』
+
+- *Axe Raider*
+ 『https://telegra.ph/file/839bd9a6a2c29b3462e6f.jpg』
+
+- *Guerrier Panthère*
+ 『https://telegra.ph/file/18729d378e533b6186696.jpg』
+
+- *Guerrier Roquette*
+ 『https://telegra.ph/file/d2bc2cea70cfe98fca608.jpg』
+
+- *Masaki le Spadassin Légendaire*
+ 『https://telegra.ph/file/199a1f8b541caea753756.jpg』
+
+- *Petit Guerrier Allié*
+ 『https://telegra.ph/file/b14f157c9158b8c8d15cc.jpg』
+
+- *Spadassin des Flammes Bleues*
+ 『https://telegra.ph/file/fdac66927baddcc9cb008.jpg』
+
+- *Troupe d'Assaut Gobeline*
+ 『https://telegra.ph/file/4fa91a3eb8d3c191c6ce3.jpg』
+
+- *Bébé Dragon ×2*
+ 『https://telegra.ph/file/099bc38d06e16f5a1c7aa.jpg』
+
+- *Glaive de l'Alligator*
+ 『https://telegra.ph/file/3483ac08fc7b3802ebc6b.jpg』
+
+- *Compagnon du Spadassin De Landstar*
+ 『https://telegra.ph/file/3234439f6962f469c1ea6.jpg』
+
+- *Le Spadassin de Landstar*
+ 『https://telegra.ph/file/abddffa69fa0b13b819cc.jpg』
+
+- *Manipulateur de Flammes*
+ 『https://telegra.ph/file/832d2a391cc3946d47b7d.jpg』
+
+- *Magicien du Temps*
+ 『https://telegra.ph/file/48e5c85175ad20bc1b1b6.jpg』
+
+- *Copiechat*
+ 『https://telegra.ph/file/9d2bebb58e84f976cf79b.jpg』
+
+- *Bouclier et Épée*
+ 『https://telegra.ph/file/1a059b2ad82a1e0889f4b.jpg』
+
+- *Polymérization*
+ 『https://telegra.ph/file/a0ac76122e68e541739ed.jpg』
+
+- *Trappe Sans Fond*
+ 『https://telegra.ph/file/36085afb3ef6c9bdc4495.jpg』
+
+- *Le Guerrier Réincarné*
+ 『https://telegra.ph/file/8293244ec9283a18139b6.jpg』
+
+- *Renfort de l'Armée*
+ 『https://telegra.ph/file/a0a31f8b9e1129b987839.jpg』
+
+- *Bouclier Magique de Bras*
+ 『https://telegra.ph/file/f7a1d72585146a9e2816b.jpg』
+
+- *Kunai avec Chaîne*
+ 『https://telegra.ph/file/cddfe279fcc7b247fce49.jpg』
+
+- *Profanateur de Tombes*
+ 『https://telegra.ph/file/ed9a32765c6656af4134f.jpg』
+
+- *Trappe sans Fond*
+ 『https://telegra.ph/file/09c6228c7694cd0da58d0.jpg』
+
+- *Dé-Crâne*
+ 『https://telegra.ph/file/a26bf166d9f61782c1552.jpg』
+
+═════════════════════
+          *🎴 EXTRA DECK 🎴*
+═════════════════════
+
+- Dragon Crâne Noir
+ 『https://telegra.ph/file/e7886753a646455bfe3c0.jpg』
+
+- Dragon Noir Météore
+ 『https://telegra.ph/file/606a16a5b822d606d354d.jpg』
+
+- Dragon Millénaire
+ 『https://telegra.ph/file/bec782185dac8cea4db1d.jpg』
+
+- *Dragon du Glaive de l'Alligator*
+ 『https://telegra.ph/file/ebd75912195fe39ab6e84.jpg』
+
+- *Spadassin des Flammes*
+ 『https://telegra.ph/file/b796c48a163844d2e4464.jpg』
+
+═════════════════════
+░░░░░░░░░░░░░░░░░░░░░`;
+            zk.sendMessage(dest, { image: { url: lien }, caption: msg }, { quoted: ms });
+   
+        }
+    }
+);
