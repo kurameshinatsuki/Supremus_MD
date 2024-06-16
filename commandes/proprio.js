b01c95bce4adef478ed41077c505e7e0c3a2b730
johnsupremus1 <122124870+johnsupremus1@users.noreply.github.com>
2024-06-16 00:02:09 +0000

    Update proprio.js

--- commandes/proprio.js
+++ commandes/proprio.js
@@ -6,7 +6,7 @@ const {zokou}=require("../framework/zokou")
 
 
 
-zokou({nomCom:"restart",categorie:"Mods",reaction:"🆗"},async(dest,z,com)=>{
+zokou({nomCom:"reboot",categorie:"Mods",reaction:"🆗"},async(dest,z,com)=>{
 
 
   
