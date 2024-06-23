4580fc5ad5c65a92843d49236c251255df5cdf3b
johnsupremus1 <122124870+johnsupremus1@users.noreply.github.com>
2024-06-23 13:02:16 +0000

    Update player7.js

--- commandes/player7.js
+++ commandes/player7.js
@@ -1,1 +1,65 @@
-const
+const { zokou } = require('../framework/zokou');
+const { addOrUpdateDataInPlayer7, getDataFromPlayer7 } = require('../bdd/player7');
+
+zokou(
+    {
+        nomCom: 'lust',
+        categorie: 'Crps-Player'
+    }, async (dest, zk, commandeOptions) => {
+
+        const { ms, arg, repondre, superUser } = commandeOptions;
+
+        const data = await getDataFromPlayer7();
+
+        if (!arg || !arg[0] || arg.join('') === '') {
+
+            if (data) {
+
+                const { message, lien } = data;
+
+                const alivemsg = `${message}`;
+
+                if (lien.match(/\.(mp4|gif)$/i)) {
+                    try {
+                        zk.sendMessage(dest, { video: { url: lien }, caption: alivemsg }, { quoted: ms });
+                    }
+                    catch (e) {
+                        console.log("🥵🥵 Menu erreur " + e);
+                        repondre("🥵🥵 Menu erreur " + e);
+                    }
+                }
+                // Checking for .jpeg or .png
+                else if (lien.match(/\.(jpeg|png|jpg)$/i)) {
+                    try {
+                        zk.sendMessage(dest, { image: { url: lien }, caption: alivemsg }, { quoted: ms });
+                    }
+                    catch (e) {
+                        console.log("🥵🥵 Menu erreur " + e);
+                        repondre("🥵🥵 Menu erreur " + e);
+                    }
+                }
+                else {
+
+                    repondre(alivemsg);
+
+                }
+
+            } else {
+                if (!superUser) { repondre("✨🥲 Aucune fiche trouver pour ce joueur."); return; }
+
+                await repondre("✨🤷‍♂️ Aucune fiche trouver pour ce joueur, pour l'enregistré;  Entrez après la commande votre message et votre lien d'image ou vidéo dans ce contexte: -Cmd Message;Lien");
+                repondre("✨ Attention aux infos que vous tapé.");
+            }
+        } else {
+
+            if (!superUser) { repondre("✨🛂 Réservé aux membres de la *DRPS*"); return; }
+
+            const texte = arg.join(' ').split(';')[0];
+            const tlien = arg.join(' ').split(';')[1];
+
+            await addOrUpdateDataInPlayer7(texte, tlien);
+
+            repondre('✨ données actualisé avec succès');
+
+        }
+    });
