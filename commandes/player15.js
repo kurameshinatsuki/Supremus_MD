051d292e6424c561b00150f41519af1c92ce580f
johnsupremus1 <122124870+johnsupremus1@users.noreply.github.com>
2024-06-27 10:19:09 +0000

    Update player15.js

--- commandes/player15.js
+++ commandes/player15.js
@@ -1,10 +1,10 @@
-/*const { zokou } = require('../framework/zokou');
+const { zokou } = require('../framework/zokou');
 const {addOrUpdateDataInPlayer15 , getDataFromPlayer15} = require('../bdd/player15')
 
 
 zokou(
     {
-        nomCom : '',
+        nomCom : 'viviane',
         categorie : 'Crps-Player'
         
     },async (dest,zk,commandeOptions) => {
