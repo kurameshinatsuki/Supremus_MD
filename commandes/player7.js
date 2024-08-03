b3180f61cc70f3ee95783eb5feca58b4dcbe4cc2
johnsupremus1 <122124870+johnsupremus1@users.noreply.github.com>
2024-08-03 06:35:11 +0000

    Update player7.js

--- commandes/player7.js
+++ commandes/player7.js
@@ -1,4 +1,4 @@
-const { zokou } = require('../framework/zokou');
+/*const { zokou } = require('../framework/zokou');
 const { addOrUpdateDataInPlayer7, getDataFromPlayer7 } = require('../bdd/player7');
 
 zokou(
