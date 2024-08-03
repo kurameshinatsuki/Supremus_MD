fb620011c9983b1a339c5396861d0ab8dd923e69
johnsupremus1 <122124870+johnsupremus1@users.noreply.github.com>
2024-08-03 15:15:40 +0000

    Update menucrps.js

--- commandes/menucrps.js
+++ commandes/menucrps.js
@@ -3,7 +3,7 @@ const { zokou } = require('../framework/zokou');
 zokou(
     {
         nomCom: 'menucrps',
-        categorie: 'crps'
+        categorie: 'Mainmenu'
     },
     async (dest, zk, commandeOptions) => {
         const { repondre, arg, ms } = commandeOptions;
@@ -39,7 +39,7 @@ Bienvenue dans le menu principal tout voir et savoir sur les activités CRPS.
 zokou(
     {
         nomCom: 'guide',
-        categorie: 'crps'
+        categorie: 'Mainmenu'
     },
     async (dest, zk, commandeOptions) => {
         const { repondre, arg, ms } = commandeOptions;
@@ -57,7 +57,7 @@ zokou(
 zokou(
     {
         nomCom: 'competition',
-        categorie: 'crps'
+        categorie: 'Mainmenu'
     },
     async (dest, zk, commandeOptions) => {
         const { repondre, arg, ms } = commandeOptions;
@@ -75,7 +75,7 @@ zokou(
 zokou(
     {
         nomCom: 'succes',
-        categorie: 'crps'
+        categorie: 'Mainmenu'
     },
     async (dest, zk, commandeOptions) => {
         const { repondre, arg, ms } = commandeOptions;
@@ -178,7 +178,7 @@ zokou(
 zokou(
     {
         nomCom: 'devise',
-        categorie: 'crps'
+        categorie: 'Mainmenu'
     },
     async (dest, zk, commandeOptions) => {
         const { repondre, arg, ms } = commandeOptions;
@@ -196,7 +196,7 @@ zokou(
 zokou(
     {
         nomCom: 'reward',
-        categorie: 'crps'
+        categorie: 'Mainmenu'
     },
     async (dest, zk, commandeOptions) => {
         const { repondre, arg, ms } = commandeOptions;
@@ -213,7 +213,7 @@ zokou(
 zokou(
     {
         nomCom: 'jobs',
-        categorie: 'crps'
+        categorie: 'Mainmenu'
     },
     async (dest, zk, commandeOptions) => {
         const { repondre, arg, ms } = commandeOptions;
@@ -230,7 +230,7 @@ zokou(
 zokou(
     {
         nomCom: 'generale',
-        categorie: 'crps'
+        categorie: 'Mainmenu'
     },
     async (dest, zk, commandeOptions) => {
         const { repondre, arg, ms } = commandeOptions;
@@ -247,7 +247,7 @@ zokou(
 zokou(
     {
         nomCom: 'menuplayer',
-        categorie: 'crps'
+        categorie: 'DRPS'
     },
     async (dest, zk, commandeOptions) => {
         const { repondre, arg, ms } = commandeOptions;
@@ -291,7 +291,7 @@ zokou(
 zokou(
     {
         nomCom: 'roliste',
-        categorie: 'crps'
+        categorie: 'DRPS'
     },
     async (dest, zk, commandeOptions) => {
         const { repondre, arg, ms } = commandeOptions;
@@ -308,16 +308,16 @@ zokou(
 > *⚜️ 𝗦𝘁𝗮𝘁𝘂𝘁 :* Rôliste✅
 
 ░░░░░░░░░░░░░░░░░░░
-> *🔥 𝗪𝗶𝘀𝗵 :*
-> *🧘‍♂️ 𝗦𝗮𝗴𝗲 :*
-> *🏆 𝗖𝗵𝗮𝗺𝗽𝗶𝗼𝗻 :*
-> *😎 𝗦𝗽𝗲𝗰𝗶𝗮𝗹𝗶𝘀𝘁𝗲 :*
-> *🏅 𝗠𝗮𝗶𝘁𝗿𝗲 :*
-> *👺 𝗗𝗶𝗰𝘁𝗮𝘁𝗲𝘂𝗿 :*
-> *🧠 𝗠𝗮𝗶𝘁𝗿𝗶𝘀𝗲 :*
-> *🤠 𝗔𝗱𝘃𝗲𝗻𝘁𝘂𝗿𝗲𝗿 :*
-> *🤼‍♂️ 𝗖𝗵𝗮𝗹𝗹𝗲𝗻𝗴𝗲 :*
-> *💯 𝗟𝗲𝗴𝗲𝗻𝗱 :*
+> *🔥 𝗪𝗶𝘀𝗵 :* 00C/01J
+> *🧘‍♂️ 𝗦𝗮𝗴𝗲 :* 000/100pts
+> *🏆 𝗖𝗵𝗮𝗺𝗽𝗶𝗼𝗻 :* D. Argent🥈
+> *😎 𝗦𝗽𝗲𝗰𝗶𝗮𝗹𝗶𝘀𝘁𝗲 :* 0/3⭐
+> *🏅 𝗠𝗮𝗶𝘁𝗿𝗲 :* 00/05V
+> *👺 𝗗𝗶𝗰𝘁𝗮𝘁𝗲𝘂𝗿 :* .../1er🥉
+> *🧠 𝗠𝗮𝗶𝘁𝗿𝗶𝘀𝗲 :* 00/10🧠
+> *🤠 𝗔𝗱𝘃𝗲𝗻𝘁𝘂𝗿𝗲𝗿 :* 00T/01J
+> *🤼‍♂️ 𝗖𝗵𝗮𝗹𝗹𝗲𝗻𝗴𝗲 :* 00/01✅
+> *💯 𝗟𝗲𝗴𝗲𝗻𝗱 :* 0⭐/0🌟/0💫
 ░░░░░░░░░░░░░░░░░░░
 > *👊 𝗙𝗶𝗴𝗵𝘁 :* 𝗩 : 000 𝗗 : 000
 > *🏅 𝗧𝗢𝗣 3 :* 000
@@ -346,7 +346,7 @@ zokou(
 zokou(
     {
         nomCom: 'fight',
-        categorie: 'crps'
+        categorie: 'Arena'
     },
     async (dest, zk, commandeOptions) => {
         const { repondre, arg, ms } = commandeOptions;
@@ -358,13 +358,13 @@ zokou(
 ═══════════════════
 >> *[Player Name]*
 
-💬:
+> 
 
->
+> 
 
-💬:
+> 
 
->
+> 
 
 *💠 𝐏𝐨𝐮𝐯𝐨𝐢𝐫 :*
 *📌 𝐃𝐢𝐬𝐭𝐚𝐧𝐜𝐞 :*
@@ -383,7 +383,7 @@ zokou(
 zokou(
     {
         nomCom: 'rule',
-        categorie: 'crps'
+        categorie: 'Arena'
     },
     async (dest, zk, commandeOptions) => {
         const { repondre, arg, ms } = commandeOptions;
@@ -396,15 +396,15 @@ zokou(
             🎮 𝐁𝐲 𝐂𝐑𝐏𝐒 🎮
 ═══════════════════
 *👤 Player 1 or Team 1 👥*
--
+ - 
                    *𝙑𝙎*
 
 *👥 Player 2 or Team 2 👥*
--
+ - 
 ░░░░░░░░░░░░░░░░░░░
 *🚫 Règles d'Affrontement :*
 *▪ WTF :* Mc
-*▪ Latence :* Section 2 annulé
+*▪ Latence :* 
 *▪ Items :*
 ░░░░░░░░░░░░░░░░░░░
 *♨️ Turner :*
@@ -416,21 +416,22 @@ zokou(
 *⭕ Statistiques :* 50m
 *⏱️ Latence :* 6 + 2min add
 ░░░░░░░░░░░░░░░░░░░
-*🌍 Environnement :*
+*🌍 Environnement :* (voir image)
 
 ░░░░░░░░░░░░░░░░░░░
 ═══════════════════
-> *✅Média immersive :* +1🧠
-> *✅Pavé immersive :* +1🧠
-> *✅Pavé spectaculaire :* +2🧠
-> *✅Gestion de stats :* +1🧠
+*La maîtrise "🧠" initial est de 6.*
+> *❌Média Descriptif :* -1🧠
+> *❌Pavé Fluide :* -1🧠
+> *❌Pavé Lassant :* -2🧠
+> *❌Stats Incorrect :* -1🧠
 ═══════════════════
-> *🥇Easy Win:* Domination.
-> *🥈Medium Win:* def +25%🫀
-> *🥉Hard Win:* def +50%🫀 & -85%❤️
-> *🏅Perfect Win:* Stats no variation.
+> *🥇Easy Win: 3:* Domination.
+> *🥈Medium Win: 2:* def +25%🫀
+> *🥉Hard Win: 1:* def +50%🫀 & -85%❤️
+> *🏅Perfect Win: 4:* Stats no variation.
 ═══════════════════
-> *⚠️ Si vous achevé l'adversaire d'un seul coup c'est un "𝗞𝗶𝗹𝗹𝗶𝗻𝗴 𝗦𝗽𝗿𝗲𝗲𝗱" +2🧠, si vous l'achevé en full power c'est "𝗥𝗮𝗺𝗽𝗮𝗴𝗲" +2🧠 et si vous gagné contre un perso de niveau supérieur c'est "𝗠𝗢𝗡𝗦𝗧𝗘𝗥 𝗞𝗜𝗟𝗟𝗘𝗥" +5🧠.*
+> *⚠️ Si vous achevé l'adversaire d'un seul coup c'est un "ONE SHOT" +2🧠, si vous l'achevé en full power c'est "RAMPAGE" +2🧠 et si vous gagné contre un perso de niveau supérieur c'est "MONSTER SLAYER" +4🧠.*
 ═══════════════════
 > *Vous avez 6️⃣ tours max pour achever votre adversaire ou décision de victoire par domination.*
 ═══════════════════
@@ -445,7 +446,7 @@ zokou(
 zokou(
     {
         nomCom: 'annonce',
-        categorie: 'crps'
+        categorie: 'Arena'
     },
     async (dest, zk, commandeOptions) => {
         const { repondre, arg, ms } = commandeOptions;
@@ -492,7 +493,7 @@ zokou(
 zokou(
     {
         nomCom: 'transact',
-        categorie: 'crps'
+        categorie: 'Transact-Zone'
     },
     async (dest, zk, commandeOptions) => {
         const { repondre, arg, ms } = commandeOptions;
@@ -523,7 +524,7 @@ zokou(
 zokou(
     {
         nomCom: 'fightrule',
-        categorie: 'crps'
+        categorie: 'Arena'
     },
     async (dest, zk, commandeOptions) => {
         const { repondre, arg, ms } = commandeOptions;
@@ -537,6 +538,154 @@ zokou(
 
             for (const lien of liens) {
                 await zk.sendMessage(dest, { image: { url: lien } }, { quoted: ms });
+            }
+        }
+    }
+);
+
+zokou(
+    {
+        nomCom: 'article',
+        categorie: 'Transact-Zone'
+    },
+    async (dest, zk, commandeOptions) => {
+        const { repondre, arg, ms } = commandeOptions;
+
+        if (!arg || arg.length === 0) {
+            // Liste des images avec leurs légendes correspondantes
+            const imagesAvecLegendes = [
+                {
+                    url: 'https://telegra.ph/file/a374b96c7674a3d6378d7.jpg',
+                    legende: '*🛍️ Article 1:* Paire de Griffes.'
+                },
+                {
+                    url: 'https://telegra.ph/file/a9c764acf27e3a235bebb.jpg',
+                    legende: '*🛍️ Article 2:* Montre.'
+                },
+                {
+                    url: 'https://telegra.ph/file/c7dc4492631033f375259.jpg',
+                    legende: '*🛍️ Article 3:* Crystal d'Amplification.'
+                }
+                {
+                    url: 'https://telegra.ph/file/ed00b10ae16a00f91c63c.jpg',
+                    legende: '*🛍️ Article 4:* Épée Ordinaire.'
+                },
+                {
+                    url: 'https://telegra.ph/file/41fe261053df5794bd732.jpg',
+                    legende: '*🛍️ Article 5:* Épée Violette.'
+                },
+                {
+                    url: 'https://telegra.ph/file/e1f2aefeeee3ff8cdfb91.jpg',
+                    legende: '*🛍️ Article 6:* Dague.'
+                }
+                {
+                    url: 'https://telegra.ph/file/49afb31ee5d5211c82e5b.jpg',
+                    legende: '*🛍️ Article 7:* Épée Terraliens.'
+                },
+                {
+                    url: 'https://telegra.ph/file/b675e1aab798f8c98ea1e.jpg',
+                    legende: '*🛍️ Article 8:* Potion Énergétique.'
+                },
+                {
+                    url: 'https://telegra.ph/file/9ac81c71047b7f5c10f65.jpg',
+                    legende: '*🛍️ Article 9:* Crystal de Communication.'
+                }
+                {
+                    url: 'https://telegra.ph/file/7929c49687b484a60145f.jpg',
+                    legende: '*🛍️ Article 10:* Potion de Soins.'
+                },
+                {
+                    url: 'https://telegra.ph/file/deae5ffc579ec14fb5642.jpg',
+                    legende: '*🛍️ Article 11:* Anneau de Charité.'
+                },
+                {
+                    url: 'https://telegra.ph/file/39dfbbc6215220cb7665d.jpg',
+                    legende: '*🛍️ Article 12:* Anneau du Lien Éternel.'
+                }
+                {
+                    url: 'https://telegra.ph/file/669f181d0b76c2889fd24.jpg',
+                    legende: '*🛍️ Article 13:* Fiole Empoisonné.'
+                },
+                {
+                    url: 'https://telegra.ph/file/86ef73e1d0d2a3d2e6585.jpg',
+                    legende: '*🛍️ Article 14:* Paire de Gants Vert.'
+                },
+                {
+                    url: 'https://telegra.ph/file/34ed2758cc6ef60cb3f8d.jpg',
+                    legende: '*🛍️ Article 15:* Pommade Médicinal.'
+                }
+                {
+                    url: 'https://telegra.ph/file/6fe610b5987c6fcd826dd.jpg',
+                    legende: '*🛍️ Article 16:* Hache.'
+                },
+                {
+                    url: 'https://telegra.ph/file/da0c204b34fff75bf751f.jpg',
+                    legende: '*🛍️ Article 17:* Arc.'
+                },
+                {
+                    url: 'https://telegra.ph/file/0996b4d4435161f804c5c.jpg',
+                    legende: '*🛍️ Article 18:* Paire d'Épée.'
+                }
+                {
+                    url: 'https://telegra.ph/file/3db859baa39b33466125c.jpg',
+                    legende: '*🛍️ Article 19:* Épée Noire.'
+                },
+                {
+                    url: 'https://telegra.ph/file/50b1e57ef0dceab1733bb.jpg',
+                    legende: '*🛍️ Article 20:* Lance.'
+                },
+                {
+                    url: 'https://telegra.ph/file/3e435b8da97f5991553e4.jpg',
+                    legende: '*🛍️ Article 21:* Nunchaku Trio.'
+                }
+                {
+                    url: 'https://telegra.ph/file/badb0bb097134299d77da.jpg',
+                    legende: '*🛍️ Article 22:* Sac à Dos.'
+                },
+                {
+                    url: 'https://telegra.ph/file/dd3d362a9a8ac03240ad0.jpg',
+                    legende: '*🛍️ Article 23:* Lance Émeraude.'
+                },
+                {
+                    url: 'https://telegra.ph/file/f74756044bab205f1b334.jpg',
+                    legende: '*🛍️ Article 24:* Fouet Épineux.'
+                }
+                {
+                    url: 'https://telegra.ph/file/c72c0f9c375c1aacb17e1.jpg',
+                    legende: '*🛍️ Article 25:* Couteau A.'
+                },
+                {
+                    url: 'https://telegra.ph/file/1acaabdfecd25e1b35e7e.jpg',
+                    legende: '*🛍️ Article 26:* Lot d'Aiguilles.'
+                },
+                {
+                    url: 'https://telegra.ph/file/6fa3ba579b0d7bb39ab19.jpg',
+                    legende: '*🛍️ Article 27:* Lance Dorée.'
+                }
+                {
+                    url: 'https://telegra.ph/file/ef301501877697ab4b416.jpg',
+                    legende: '*🛍️ Article 28:* Couteau Croissant.'
+                },
+                {
+                    url: 'https://telegra.ph/file/cbe78a622c4468c6b2040.jpg',
+                    legende: '*🛍️ Article 29:* Fouet à pointe.'
+                },
+                {
+                    url: 'https://telegra.ph/file/2758cb958e2a5e8d79561.jpg',
+                    legende: '*🛍️ Article 30:* Fouet à Rubans.'
+                }
+            ];
+
+            // Envoi des images avec légendes
+            for (const image of imagesAvecLegendes) {
+                await zk.sendMessage(
+                    dest,
+                    {
+                        image: { url: image.url },
+                        caption: image.legende
+                    },
+                    { quoted: ms }
+                );
             }
         }
     }
