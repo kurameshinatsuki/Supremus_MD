da4a9d10c8061cf1abcbeb84f2c4fbbf31e6985e
johnsupremus1 <122124870+johnsupremus1@users.noreply.github.com>
2024-10-03 13:51:16 +0000

    Update catalogue_abm.js

--- commandes/catalogue_abm.js
+++ commandes/catalogue_abm.js
@@ -1,36 +1,63 @@
 const { zokou } = require('../framework/zokou');
 
-// Dictionnaire des personnages et leurs images par univers
+// Dictionnaire des personnages avec leurs liens et rangs par univers
 const personnages = {
     'black_clover': {
-        ASTA: 'https://i.ibb.co/b7nG8PT/Image-2024-09-22-23-51-04-0.jpg',
-        YUNO: 'https://i.ibb.co/gdTzR95/Image-2024-09-22-23-51-04-1.jpg',
-        LÉOPOLD: 'https://i.ibb.co/BzQ41L5/Image-2024-09-22-23-51-04-5.jpg',
-        GAUCHE: 'https://i.ibb.co/dtdqN3v/Image-2024-09-22-23-51-04-6.jpg',
-        MAGNA: 'https://i.ibb.co/j580P6c/Image-2024-09-22-23-51-04-3.jpg',
-        LUCK: 'https://i.ibb.co/y6ssxQ0/Image-2024-09-22-23-51-04-4.jpg',
-        NOELLE: 'https://i.ibb.co/23kcHZx/Image-2024-09-22-23-51-04-2.jpg',
-        VANESSA: 'https://i.ibb.co/RQpvRyD/Image-2024-09-22-23-51-04-7.jpg',
-        ZORA: 'https://i.ibb.co/1MGtpKF/Image-2024-09-22-23-51-04-8.jpg',
-        LANGRIS: 'https://i.ibb.co/GvNHQ7R/Image-2024-09-22-23-51-04-9.jpg',
+        NOELLE: { lien: 'https://i.ibb.co/kMbkzL9/Image-2024-10-02-19-26-48-3.jpg', rang: 'C' },
+        MAGNA: { lien: 'https://i.ibb.co/DfDv87k/Image-2024-10-02-19-26-48-2.jpg', rang: 'C' },
+        GAUCHE: { lien: 'https://i.ibb.co/KFNVJ9h/Image-2024-10-02-19-26-48-4.jpg', rang: 'C' },
+        YUNO: { lien: 'https://i.ibb.co/K7yv6pw/Image-2024-10-02-19-26-48-1.jpg', rang: 'C' },
+        VANESSA: { lien: 'https://i.ibb.co/DDSGBnH/Image-2024-10-02-19-26-48-7.jpg', rang: 'C' },
+        ZORA: { lien: 'https://i.ibb.co/0cDXhjK/Image-2024-10-02-19-26-48-8.jpg', rang: 'C' },
+        LANGRIS: { lien: 'https://i.ibb.co/rGbpzD4/Image-2024-10-02-19-26-48-9.jpg', rang: 'C' },
+        LEOPOLD: { lien: 'https://i.ibb.co/Cw3yfjD/Image-2024-10-02-19-26-48-6.jpg', rang: 'C' },
+        LUCK: { lien: 'https://i.ibb.co/PMwq7ZC/Image-2024-10-02-19-26-48-5.jpg', rang: 'C' },
+        ASTA: { lien: 'https://i.ibb.co/khfFhfQ/Image-2024-10-02-19-26-48-0.jpg', rang: 'C' },
     },
     'demon_slayer': {
-        RIU: 'https://i.ibb.co/k0jFqQj/Image-2024-09-23-16-51-29-8.jpg',
-        GIYU: 'https://i.ibb.co/k9xb9VG/Image-2024-09-23-16-51-29-2.jpg',
-        ZENITSU: 'https://i.ibb.co/BqbZ7VN/Image-2024-09-23-16-51-29-3.jpg',
-        ENMU: 'https://i.ibb.co/1r3g39z/Image-2024-09-23-16-51-29-7.jpg',
-        SUSAMARU: 'https://i.ibb.co/VggXmkz/Image-2024-09-23-16-51-29-6.jpg',
-        YAHABA: 'https://i.ibb.co/hBcm3gf/Image-2024-09-23-16-51-29-5.jpg',
-        NEZUKO: 'https://i.ibb.co/h8Jt7JG/Image-2024-09-23-16-51-29-1.jpg',
-        INOSUKE: 'https://i.ibb.co/LZ1nWyM/Image-2024-09-23-16-51-29-4.jpg',
-        TANJIRO: 'https://i.ibb.co/fnzKSqB/Image-2024-09-23-16-51-29-0.jpg',
+        TANJIRO_2: { lien: 'https://i.ibb.co/K9xHhtJ/Image-2024-10-02-19-51-46-8.jpg', rang: 'C' },
+        ENMU: { lien: 'https://i.ibb.co/gVCfKsX/Image-2024-10-02-19-51-46-7.jpg', rang: 'C' },
+        RIU: { lien: 'https://i.ibb.co/7gh2Hvz/Image-2024-10-02-19-51-46-6.jpg', rang: 'C' },
+        NEZUKO: { lien: 'https://i.ibb.co/DGMgsBR/Image-2024-10-02-19-51-46-1.jpg', rang: 'C' },
+        INOSUKE: { lien: 'https://i.ibb.co/2PsDh85/Image-2024-10-02-19-51-46-3.jpg', rang: 'C' },
+        ZENITSU: { lien: 'https://i.ibb.co/nQZt1nM/Image-2024-10-02-19-51-46-2.jpg', rang: 'C' },
+        SUSAMARU: { lien: 'https://i.ibb.co/JsfJKSr/Image-2024-10-02-19-51-46-5.jpg', rang: 'C' },
+        TANJIRO: { lien: 'https://i.ibb.co/NsZp3mb/Image-2024-10-02-19-51-46-0.jpg', rang: 'C' },
+        KANAO: { lien: 'https://i.ibb.co/FxRz0Kx/Image-2024-10-02-19-51-46-9.jpg', rang: 'C' },
+        YAHABA: { lien: 'https://i.ibb.co/SNhjnbW/Image-2024-10-02-19-51-46-4.jpg', rang: 'C' },
+    },
+    'jujutsu_kaisen': {
+        INO: { lien: 'https://i.ibb.co/6HSKdfR/Image-2024-10-02-20-05-39-13.jpg', rang: 'C' },
+        GETO: { lien: 'https://i.ibb.co/dgn0FHn/Image-2024-10-02-20-05-39-11.jpg', rang: 'C' },
+        MEGUMI: { lien: 'https://i.ibb.co/VMXWTgh/Image-2024-10-02-20-05-39-1.jpg', rang: 'C' },
+        MAI: { lien: 'https://i.ibb.co/RH46BwN/Image-2024-10-02-20-05-39-9.jpg', rang: 'C' },
+        MAHITO: { lien: 'https://i.ibb.co/cgySkn4/Image-2024-10-02-20-05-39-6.jpg', rang: 'C' },
+        NANAMI: { lien: 'https://i.ibb.co/j6GrFtm/Image-2024-10-02-20-05-39-4.jpg', rang: 'C' },
+        MECHAMARU: { lien: 'https://i.ibb.co/cgdRNfG/Image-2024-10-02-20-05-39-12.jpg', rang: 'C' },
+        YUJI: { lien: 'https://i.ibb.co/kGrfKgW/Image-2024-10-02-20-05-39-0.jpg', rang: 'C' },
+        KAMO: { lien: 'https://i.ibb.co/XVdST0Y/Image-2024-10-02-20-05-39-14.jpg', rang: 'C' },
+        AOI: { lien: 'https://i.ibb.co/FDqvc7r/Image-2024-10-02-20-05-39-2.jpg', rang: 'C' },
+        CHOSO: { lien: 'https://i.ibb.co/27fNC4H/Image-2024-10-02-20-05-39-10.jpg', rang: 'C' },
+        MIWA: { lien: 'https://i.ibb.co/vd0FNTL/Image-2024-10-02-20-05-39-7.jpg', rang: 'C' },
+        TOGE: { lien: 'https://i.ibb.co/kMVTKLy/Image-2024-10-02-20-05-39-8.jpg', rang: 'C' },
+        NOBARA: { lien: 'https://i.ibb.co/znZCXYX/Image-2024-10-02-20-05-39-3.jpg', rang: 'C' },
+        MAKI: { lien: 'https://i.ibb.co/4Vbv31Q/Image-2024-10-02-20-05-39-5.jpg', rang: 'C' },
     }
 };
 
 // Fonction pour envoyer la carte du personnage
 async function envoyerCarte(dest, zk, ms, verse, personnage) {
-    const lien = personnages[verse][personnage];
-    if (lien) {
+    const verseData = personnages[verse];
+
+    if (!verseData) {
+        zk.sendMessage(dest, { text: `Verse ${verse} non trouvé.` }, { quoted: ms });
+        return;
+    }
+
+    const personnageInfo = verseData[personnage.toUpperCase()];
+
+    if (personnageInfo) {
+        const { lien } = personnageInfo;
         zk.sendMessage(dest, { image: { url: lien }, caption: personnage }, { quoted: ms });
     } else {
         zk.sendMessage(dest, { text: `Personnage ${personnage} non trouvé dans le verse ${verse}.` }, { quoted: ms });
@@ -49,7 +76,7 @@ zokou(
         if (!arg || arg.length === 0) {
             zk.sendMessage(dest, { text: 'Veuillez spécifier un personnage.' }, { quoted: ms });
         } else {
-            const personnage = arg[0].toUpperCase(); // Nom du personnage en majuscule
+            const personnage = arg[0];
             await envoyerCarte(dest, zk, ms, 'black_clover', personnage);
         }
     }
@@ -67,8 +94,26 @@ zokou(
         if (!arg || arg.length === 0) {
             zk.sendMessage(dest, { text: 'Veuillez spécifier un personnage.' }, { quoted: ms });
         } else {
-            const personnage = arg[0].toUpperCase();
+            const personnage = arg[0];
             await envoyerCarte(dest, zk, ms, 'demon_slayer', personnage);
+        }
+    }
+);
+
+// Commande pour Jujutsu Kaisen
+zokou(
+    {
+        nomCom: 'jujutsu_kaisen',
+        categorie: 'ABM'
+    },
+    async (dest, zk, commandeOptions) => {
+        const { arg, ms } = commandeOptions;
+
+        if (!arg || arg.length === 0) {
+            zk.sendMessage(dest, { text: 'Veuillez spécifier un personnage.' }, { quoted: ms });
+        } else {
+            const personnage = arg[0];
+            await envoyerCarte(dest, zk, ms, 'jujutsu_kaisen', personnage);
         }
     }
 );
