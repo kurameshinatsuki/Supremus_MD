3b8e07eda3f8349191036b376938c04ef19674b0
kurameshi natsuki <111725926+kurameshinatsuki@users.noreply.github.com>
2024-10-17 16:49:05 +0000

    Update test.js

--- commandes/test.js
+++ commandes/test.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const { zokou } = require("../framework/zokou");
 zokou({ nomCom: "test", reaction: "😌", nomFichier: __filename, categorie: 'MON-BOT' }, async (dest, zk, commandeOptions) => {
     console.log("Commande saisie !!!s");
-    let z = 'Salut je suis *SP-ZK-MD* un bot WhatsApp dynamique et tout droit réserver au service de la *🪀 Supremus Prod 🪀*\n\n";
+    let z = "Salut je suis *SP-ZK-MD* un bot WhatsApp dynamique et tout droit réserver au service de la *🪀 Supremus Prod 🪀*\n\n";
     let d = 'développer par *Jøhñ Sũpręmũs*';
     let varmess = z + d;
     var img = 'https://i.ibb.co/k81T2WX/image.jpg';
