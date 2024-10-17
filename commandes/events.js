5275b27369a42909ff3d7994ed8e85668d0d974d
kurameshi natsuki <111725926+kurameshinatsuki@users.noreply.github.com>
2024-10-17 08:47:19 +0000

    Update events.js

--- commandes/events.js
+++ commandes/events.js
@@ -4,7 +4,7 @@ const { attribuerUnevaleur } = require('../bdd/welcome');
 async function events(nomCom) {
     zokou({
         nomCom: nomCom,
-        categorie: 'Group'
+        categorie: 'GROUP'
     }, async (dest, zk, commandeOptions) => {
         const { ms, arg, repondre, superUser, verifAdmin } = commandeOptions;
 
