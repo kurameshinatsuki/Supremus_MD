1caa1f240d7a0ee17c32780e5a85c68d88cfec6e
kurameshi natsuki <111725926+kurameshinatsuki@users.noreply.github.com>
2024-10-24 18:15:10 +0000

    Update player_bdd.js

--- bdd/player_bdd.js
+++ bdd/player_bdd.js
@@ -19,8 +19,7 @@ async function createTables() {
   try {
     await client.query(`
       CREATE TABLE IF NOT EXISTS player_profiles(
-        id SERIAL PRIMARY KEY,
-        username TEXT NOT NULL,
+        name TEXT PRIMARY KEY,
         statut TEXT DEFAULT 'Novice',
         mode TEXT DEFAULT 'Free',
         rang_abm TEXT DEFAULT 'aucun',
@@ -30,48 +29,30 @@ async function createTables() {
         specialite TEXT DEFAULT 'aucune',
         leader TEXT DEFAULT 'aucun',
         defis_remportes INTEGER DEFAULT 0,
-        legende TEXT DEFAULT 'aucune'
-      );
-    `);
-
-    await client.query(`
-      CREATE TABLE IF NOT EXISTS player_stats(
-        player_id INTEGER REFERENCES player_profiles(id),
+        legende TEXT DEFAULT 'aucune',
         victoires INTEGER DEFAULT 0,
         defaites INTEGER DEFAULT 0,
         forfaits INTEGER DEFAULT 0,
         top3 INTEGER DEFAULT 0,
         missions_reussies INTEGER DEFAULT 0,
         missions_echouees INTEGER DEFAULT 0,
-        PRIMARY KEY (player_id)
-      );
-    `);
-
-    await client.query(`
-      CREATE TABLE IF NOT EXISTS player_heroes(
-        player_id INTEGER REFERENCES player_profiles(id),
         amb_cards TEXT DEFAULT 'aucun',
         vehicles TEXT DEFAULT 'aucun',
         yugioh_deck TEXT DEFAULT 'aucun',
         skins TEXT DEFAULT 'aucun',
         items TEXT DEFAULT 'aucun',
-        PRIMARY KEY (player_id)
-      );
-    `);
-
-    await client.query(`
-      CREATE TABLE IF NOT EXISTS player_currency(
-        player_id INTEGER REFERENCES player_profiles(id),
         s_tokens INTEGER DEFAULT 0,
         s_gemmes INTEGER DEFAULT 0,
         coupons INTEGER DEFAULT 0,
         box_vip INTEGER DEFAULT 0,
-        compteur INTEGER DEFAULT 0,
-        PRIMARY KEY (player_id)
+        depenses INTEGER DEFAULT 0,
+        profits INTEGER DEFAULT 0,
+        retraits INTEGER DEFAULT 0,
+        solde INTEGER DEFAULT 0
       );
     `);
 
-    console.log('Tables créées avec succès');
+    console.log('Table créée avec succès');
   } catch (error) {
     console.error('Erreur lors de la création des tables:', error);
   } finally {
@@ -80,23 +61,17 @@ async function createTables() {
 }
 
 // Fonction pour insérer un profil de joueur
-async function insertPlayerProfile(username) {
+async function insertPlayerProfile(name) {
   const client = await pool.connect();
 
   try {
     const query = `
-      INSERT INTO player_profiles(username)
-      VALUES ($1) RETURNING id;
+      INSERT INTO player_profiles(name)
+      VALUES ($1);
     `;
-    const result = await client.query(query, [username]);
+    await client.query(query, [name]);
 
-    const playerId = result.rows[0].id;
-
-    await client.query(`INSERT INTO player_stats(player_id) VALUES ($1)`, [playerId]);
-    await client.query(`INSERT INTO player_heroes(player_id) VALUES ($1)`, [playerId]);
-    await client.query(`INSERT INTO player_currency(player_id) VALUES ($1)`, [playerId]);
-
-    console.log(`Profil du joueur créé avec succès : ID ${playerId}`);
+    console.log(`Profil du joueur créé avec succès : Nom ${name}`);
   } catch (error) {
     console.error('Erreur lors de la création du profil du joueur:', error);
   } finally {
@@ -105,32 +80,17 @@ async function insertPlayerProfile(username) {
 }
 
 // Fonction pour récupérer les données du joueur
-async function getPlayerProfile(identifier) {
+async function getPlayerProfile(name) {
   const client = await pool.connect();
 
   try {
-    let query;
-    if (isNaN(identifier)) {
-      query = `
-        SELECT p.*, s.*, h.*, c.*
-        FROM player_profiles p
-        LEFT JOIN player_stats s ON p.id = s.player_id
-        LEFT JOIN player_heroes h ON p.id = h.player_id
-        LEFT JOIN player_currency c ON p.id = c.player_id
-        WHERE p.username = $1;
-      `;
-    } else {
-      query = `
-        SELECT p.*, s.*, h.*, c.*
-        FROM player_profiles p
-        LEFT JOIN player_stats s ON p.id = s.player_id
-        LEFT JOIN player_heroes h ON p.id = h.player_id
-        LEFT JOIN player_currency c ON p.id = c.player_id
-        WHERE p.id = $1;
-      `;
-    }
+    const query = `
+      SELECT *
+      FROM player_profiles
+      WHERE name = $1;
+    `;
 
-    const result = await client.query(query, [identifier]);
+    const result = await client.query(query, [name]);
 
     if (result.rows.length === 0) {
       console.log("Joueur non trouvé");
@@ -146,7 +106,7 @@ async function getPlayerProfile(identifier) {
 }
 
 // Fonction pour mettre à jour le profil du joueur
-async function updatePlayerProfile(username, updates) {
+async function updatePlayerProfile(name, updates) {
   const client = await pool.connect();
 
   try {
@@ -164,14 +124,14 @@ async function updatePlayerProfile(username, updates) {
     const query = `
       UPDATE player_profiles
       SET ${setClauses.join(', ')}
-      WHERE username = $${index}
+      WHERE name = $${index}
     `;
 
-    values.push(username);
+    values.push(name);
 
     await client.query(query, values);
 
-    console.log(`Profil du joueur ${username} mis à jour avec succès`);
+    console.log(`Profil du joueur ${name} mis à jour avec succès`);
   } catch (error) {
     console.error('Erreur lors de la mise à jour du profil du joueur:', error);
   } finally {
