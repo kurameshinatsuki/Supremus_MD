e769e8773ae4570d5934a884a7133e464a599924
kurameshi natsuki <111725926+kurameshinatsuki@users.noreply.github.com>
2024-10-25 05:45:12 +0000

    Update commandes

---
+++ commandes/teste.js
@@ -0,0 +1,20 @@
+const { zokou } = require('../framework/zokou');
+const { characters_rang_c } = require('../catalogue_abm')
+
+// Commande générale pour tous les univers
+zokou(
+    {
+        nomCom: 'heroes',
+        categorie: 'ABM'
+    },
+    async (dest, zk, commandeOptions) => {
+        const { arg, ms } = commandeOptions;
+
+        if (!arg && arg.length === 0) {
+            zk.sendMessage(dest, { text: 'Veuillez spécifier un personnage.' }, { quoted: ms });
+        } else {
+            const personnage = arg[0];
+            await envoyerCarte(dest, zk, ms, personnage);
+        }
+    }
+);
