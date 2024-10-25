f748267fae7a3c4c9e4fd24b6393573f0106b61d
kurameshi natsuki <111725926+kurameshinatsuki@users.noreply.github.com>
2024-10-25 06:55:08 +0000

    Update teste.js

--- commandes/teste.js
+++ commandes/teste.js
@@ -1,6 +1,28 @@
 const { zokou } = require('../framework/zokou');
 const { characters_rang_c } = require('../commandes/catalogue_abm')
 
+// Fonction pour envoyer la carte du personnage
+async function envoyerCarte(dest, zk, ms, personnage) {
+    let personnageTrouve = false;
+
+    // Parcourir tous les univers pour trouver le personnage
+    for (const [verse, personnages] of Object.entries(characters_rang_c)) {
+        const personnageInfo = personnages[personnage.toUpperCase()];
+
+        if (personnageInfo) {
+            personnageTrouve = true;
+            const { lien } = personnageInfo;
+            zk.sendMessage(dest, { image: { url: lien }, caption: `${personnage} (${verse})` }, { quoted: ms });
+            break;  // Si le personnage est trouvé, on arrête la boucle
+        }
+    }
+
+    // Si le personnage n'a pas été trouvé dans aucun univers
+    if (!personnageTrouve) {
+        zk.sendMessage(dest, { text: `Personnage ${personnage} non trouvé dans aucun univers.` }, { quoted: ms });
+    }
+}
+
 // Commande générale pour tous les univers
 zokou(
     {
