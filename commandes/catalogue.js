2d4be18baf3b21223d030a1abfb0c3a8aa0adf42
kurameshi natsuki <111725926+kurameshinatsuki@users.noreply.github.com>
2025-02-16 16:35:40 +0000

    Update commandes

---
+++ commandes/catalogue.js
@@ -0,0 +1,29 @@
+const characters = {
+    'C': {
+        'BLACK_CLOVER': {
+            'ASTA': { lien: 'https://i.ibb.co/khfFhfQ/Image-2024-10-02-19-26-48-0.jpg' },
+            'NOELLE': { lien: 'https://i.ibb.co/kMbkzL9/Image-2024-10-02-19-26-48-3.jpg' },
+            'MAGNA': { lien: 'https://i.ibb.co/DfDv87k/Image-2024-10-02-19-26-48-2.jpg' }
+        },
+        'DEMON_SLAYER': {
+            'TANJIRO': { lien: 'https://i.ibb.co/NsZp3mb/Image-2024-10-02-19-51-46-0.jpg' },
+            'NEZUKO': { lien: 'https://i.ibb.co/DGMgsBR/Image-2024-10-02-19-51-46-1.jpg' },
+            'ZENITSU': { lien: 'https://i.ibb.co/nQZt1nM/Image-2024-10-02-19-51-46-2.jpg' }
+        },
+        'JUJUTSU_KAISEN': {
+            'YUJI': { lien: 'https://i.ibb.co/kGrfKgW/Image-2024-10-02-20-05-39-0.jpg' },
+            'MEGUMI': { lien: 'https://i.ibb.co/VMXWTgh/Image-2024-10-02-20-05-39-1.jpg' },
+            'NOBARA': { lien: 'https://i.ibb.co/znZCXYX/Image-2024-10-02-20-05-39-3.jpg' }
+        }
+    },
+    'B': {
+        'FAIRYTAIL': {
+            'NATSU': { lien: 'https://i.ibb.co/1GjBFZBY/Image-2025-02-12-16-42-49-0.jpg' },
+            'ERZA': { lien: 'https://i.ibb.co/DNr8tTK/Image-2025-02-12-16-42-49-1.jpg' }
+        }
+    }
+};
+
+module.exports = {
+    characters
+};
