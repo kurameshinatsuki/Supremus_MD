e4bb842df64f2e409e16373cb2fec5af8cc0c4f7
kurameshi natsuki <111725926+kurameshinatsuki@users.noreply.github.com>
2025-02-16 16:56:37 +0000

    Update catalogue.js

--- commandes/catalogue.js
+++ commandes/catalogue.js
@@ -1,19 +1,47 @@
+// Dictionnaire des personnages avec leurs liens et rangs par univers
+
 const characters = {
     'C': {
         'BLACK_CLOVER': {
             'ASTA': { lien: 'https://i.ibb.co/khfFhfQ/Image-2024-10-02-19-26-48-0.jpg' },
             'NOELLE': { lien: 'https://i.ibb.co/kMbkzL9/Image-2024-10-02-19-26-48-3.jpg' },
-            'MAGNA': { lien: 'https://i.ibb.co/DfDv87k/Image-2024-10-02-19-26-48-2.jpg' }
+            'MAGNA': { lien: 'https://i.ibb.co/DfDv87k/Image-2024-10-02-19-26-48-2.jpg' },
+            'GAUCHE': { lien: 'https://i.ibb.co/KFNVJ9h/Image-2024-10-02-19-26-48-4.jpg' },
+            'YUNO': { lien: 'https://i.ibb.co/K7yv6pw/Image-2024-10-02-19-26-48-1.jpg' },
+            'VANESSA': { lien: 'https://i.ibb.co/DDSGBnH/Image-2024-10-02-19-26-48-7.jpg' },
+            'ZORA': { lien: 'https://i.ibb.co/0cDXhjK/Image-2024-10-02-19-26-48-8.jpg' },
+            'LANGRIS': { lien: 'https://i.ibb.co/rGbpzD4/Image-2024-10-02-19-26-48-9.jpg' },
+            'LEOPOLD': { lien: 'https://i.ibb.co/Cw3yfjD/Image-2024-10-02-19-26-48-6.jpg' },
+            'LUCK': { lien: 'https://i.ibb.co/PMwq7ZC/Image-2024-10-02-19-26-48-5.jpg' }
         },
         'DEMON_SLAYER': {
             'TANJIRO': { lien: 'https://i.ibb.co/NsZp3mb/Image-2024-10-02-19-51-46-0.jpg' },
             'NEZUKO': { lien: 'https://i.ibb.co/DGMgsBR/Image-2024-10-02-19-51-46-1.jpg' },
-            'ZENITSU': { lien: 'https://i.ibb.co/nQZt1nM/Image-2024-10-02-19-51-46-2.jpg' }
+            'ZENITSU': { lien: 'https://i.ibb.co/nQZt1nM/Image-2024-10-02-19-51-46-2.jpg' },
+            'INOSUKE': { lien: 'https://i.ibb.co/2PsDh85/Image-2024-10-02-19-51-46-3.jpg' },
+            'YAHABA': { lien: 'https://i.ibb.co/SNhjnbW/Image-2024-10-02-19-51-46-4.jpg' },
+            'SUSAMARU': { lien: 'https://i.ibb.co/JsfJKSr/Image-2024-10-02-19-51-46-5.jpg' },
+            'RIU': { lien: 'https://i.ibb.co/7gh2Hvz/Image-2024-10-02-19-51-46-6.jpg' },
+            'ENMU': { lien: 'https://i.ibb.co/gVCfKsX/Image-2024-10-02-19-51-46-7.jpg' },
+            'TANJIRO_2': { lien: 'https://i.ibb.co/K9xHhtJ/Image-2024-10-02-19-51-46-8.jpg' },
+            'KANAO': { lien: 'https://i.ibb.co/FxRz0Kx/Image-2024-10-02-19-51-46-9.jpg' }
         },
         'JUJUTSU_KAISEN': {
             'YUJI': { lien: 'https://i.ibb.co/kGrfKgW/Image-2024-10-02-20-05-39-0.jpg' },
             'MEGUMI': { lien: 'https://i.ibb.co/VMXWTgh/Image-2024-10-02-20-05-39-1.jpg' },
-            'NOBARA': { lien: 'https://i.ibb.co/znZCXYX/Image-2024-10-02-20-05-39-3.jpg' }
+            'AOI': { lien: 'https://i.ibb.co/FDqvc7r/Image-2024-10-02-20-05-39-2.jpg' },
+            'NOBARA': { lien: 'https://i.ibb.co/znZCXYX/Image-2024-10-02-20-05-39-3.jpg' },
+            'NANAMI': { lien: 'https://i.ibb.co/j6GrFtm/Image-2024-10-02-20-05-39-4.jpg' },
+            'MAKI': { lien: 'https://i.ibb.co/4Vbv31Q/Image-2024-10-02-20-05-39-5.jpg' },
+            'MAHITO': { lien: 'https://i.ibb.co/cgySkn4/Image-2024-10-02-20-05-39-6.jpg' },
+            'MIWA': { lien: 'https://i.ibb.co/vd0FNTL/Image-2024-10-02-20-05-39-7.jpg' },
+            'TOGE': { lien: 'https://i.ibb.co/kMVTKLy/Image-2024-10-02-20-05-39-8.jpg' },
+            'MAI': { lien: 'https://i.ibb.co/RH46BwN/Image-2024-10-02-20-05-39-9.jpg' },
+            'CHOSO': { lien: 'https://i.ibb.co/27fNC4H/Image-2024-10-02-20-05-39-10.jpg' },
+            'GETO': { lien: 'https://i.ibb.co/dgn0FHn/Image-2024-10-02-20-05-39-11.jpg' },
+            'MECHAMARU': { lien: 'https://i.ibb.co/cgdRNfG/Image-2024-10-02-20-05-39-12.jpg' },
+            'INO': { lien: 'https://i.ibb.co/6HSKdfR/Image-2024-10-02-20-05-39-13.jpg' },
+            'KAMO': { lien: 'https://i.ibb.co/XVdST0Y/Image-2024-10-02-20-05-39-14.jpg' }
         }
     },
     'B': {
