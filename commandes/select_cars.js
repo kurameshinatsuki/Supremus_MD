ee881c2da1f1c3a1ba9bd76c30dd74ea9163a29f
kurameshi natsuki <111725926+kurameshinatsuki@users.noreply.github.com>
2025-02-17 18:31:31 +0000

    Update commandes

---
+++ commandes/select_cars.js
@@ -0,0 +1,31 @@
+// Dictionnaire des engins avec leurs liens et catégorie
+
+const select_cars = {
+    'SUPERCARS': {
+        'PREMUIM': {
+            'BUGATTI_CHIRON': { lien: 'https://i.ibb.co/8ncyNX8S/Image-2025-02-17-13-54-01-6.jpg' },
+            'MCLAREN_P1': { lien: 'https://i.ibb.co/TnG8sT3/Image-2025-02-17-13-54-01-7.jpg' }
+        },
+        'STANDARD': {
+            'LAMBORGHINI_AVENTADOR': { lien: 'https://i.ibb.co/MyBYFBqM/Image-2025-02-17-13-54-01-1.jpg' },
+            'FERRARI_SF90_STRADALE': { lien: 'https://i.ibb.co/3xKZSQ1/Image-2025-02-17-13-54-01-0.jpg' },
+            'PORSCHE_911_TURBO_S': { lien: 'https://i.ibb.co/Dggyqtyx/Image-2025-02-17-13-54-01-5.jpg' }
+        }
+    },
+
+    'MOTOS': {
+        'PREMUIM': {
+            'YAMAHA_YZF-R1': { lien: 'https://i.ibb.co/hJF7mVjD/Image-2025-02-17-13-54-02-8.jpg' },
+            'BMW_S1000RR': { lien: 'https://i.ibb.co/S4jb040f/Image-2025-02-17-13-54-01-4.jpg' }
+        },
+        'STANDARD': {
+            'KAWASAKI_NINJA_H2R': { lien: 'https://i.ibb.co/20y2cRs/Image-2025-02-17-13-54-02-9.jpg' },
+            'DUCATI_PANIGALE_V4': { lien: 'https://i.ibb.co/Hfshwqsb/Image-2025-02-17-13-54-01-3.jpg' },
+            'HONDA_CBR1000RR-R': { lien: 'https://i.ibb.co/G4w850Sn/Image-2025-02-17-13-54-01-2.jpg' }
+        }
+    }
+};
+
+module.exports = {
+    select_cars
+};
