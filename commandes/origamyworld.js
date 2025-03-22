67a95d2db51ec3cd0d10c2285c9de8a4e96d7cbf
SupremusJohn <164743963+SupremusJohn@users.noreply.github.com>
2025-03-22 01:04:38 +0000

    Update origamyworld.js

--- commandes/origamyworld.js
+++ commandes/origamyworld.js
@@ -440,3 +440,57 @@ zokou(
         }
     }
 );
+
+zokou(
+    { nomCom: 'astoria_visuel', categorie: 'CENTRAL' },
+    async (dest, zk, commandeOptions) => {
+        const { ms, superUser, repondre } = commandeOptions;
+
+        // Vérification si l'utilisateur est un super utilisateur
+        if (!superUser) {
+            return repondre("❌ Accès refusé : vous n'êtes pas autorisé à exécuter cette commande !");
+        }
+
+        // Liste des liens d'images
+        const liens = [
+            'https://i.ibb.co/LtFzy6j/Image-2024-10-05-12-16-43.jpg',
+            'https://i.ibb.co/G3ztCpW/20240927-230914.jpg',
+            'https://i.ibb.co/vmN0SSr/20240927-231229.jpg',
+            'https://i.ibb.co/mBqrG20/20240927-233225.jpg',
+            'https://i.ibb.co/G5jPJN8/20240927-233347.jpg',
+            'https://i.ibb.co/51qmnJJ/20240927-233900.jpg',
+            'https://i.ibb.co/bJPbxW2/20240927-230107.jpg',
+            'https://i.ibb.co/4m005vx/20240927-233715.jpg',
+            'https://i.ibb.co/0YkNDvc/20240927-230702.jpg',
+            'https://i.ibb.co/HGhxgDs/20240928-001444.jpg',
+            'https://i.ibb.co/jv8q587/20240927-234214.jpg',
+            'https://i.ibb.co/QmMF8B6/20240927-223830.jpg',
+            'https://i.ibb.co/VgCPhyd/20240928-000526.jpg',
+            'https://i.ibb.co/zX3NZrR/20240927-234341.jpg',
+            'https://i.ibb.co/nBZ08Lh/20240927-224242.jpg',
+            'https://i.ibb.co/sj9z6jC/20240928-000853.jpg',
+            'https://i.ibb.co/fCRgqwy/20240928-001305.jpg',
+            'https://i.ibb.co/MpxhHrd/20240927-212108.jpg',
+            'https://i.ibb.co/RCpMXYj/20240927-234545.jpg',
+            'https://i.ibb.co/5WjszYy/20240927-221021.jpg',
+            'https://i.ibb.co/5Tr77gw/20240927-235428.jpg',
+            'https://i.ibb.co/L091WtQ/20240927-222537.jpg',
+            'https://i.ibb.co/j8R23mF/20240927-235952.jpg',
+            'https://i.ibb.co/0MXQjcy/20240927-222739.jpg',
+            'https://i.ibb.co/t2Txdd8/20240928-000303.jpg',
+            'https://i.ibb.co/QYzgXNg/20240928-001822.jpg',
+            'https://i.ibb.co/WvfbbgK/20240927-223020.jpg',
+            'https://i.ibb.co/3mcQzpb/20240927-235656.jpg',
+            'https://i.ibb.co/4dKMmWq/20240927-224809.jpg',
+            'https://i.ibb.co/MVFJzh1/20240927-223321.jpg',
+            'https://i.ibb.co/thkwBjn/20240927-234927.jpg',
+            'https://i.ibb.co/Kh3JdMK/20240927-221342.jpg',
+            'https://i.ibb.co/tKQCYHb/20240927-223933.jpg'
+        ];
+
+        // Boucle pour envoyer chaque image
+        for (const lien of liens) {
+            await envoyerImage(dest, zk, ms, lien);
+        }
+    }
+);
