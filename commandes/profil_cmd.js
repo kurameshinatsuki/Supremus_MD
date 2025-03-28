3dbf0e4a4c7c023ee164e221ff9d67706f6630f4
SupremusJohn <164743963+SupremusJohn@users.noreply.github.com>
2025-03-28 21:02:19 +0000

    Update profil_cmd.js

--- commandes/profil_cmd.js
+++ commandes/profil_cmd.js
@@ -1,4 +1,4 @@
-const { zokou } = require('../framework/zokou');
+/*const { zokou } = require('../framework/zokou');
 const { getVerdictByKeyword, updateVerdict } = require('../bdd/profil_bdd');
 
 zokou(
@@ -77,4 +77,4 @@ zokou(
             repondre("Une erreur est survenue. Veuillez réessayer.");
         }
     }
-);
+);*/
