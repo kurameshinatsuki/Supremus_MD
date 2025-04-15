5cd533e2ccb57909abef7312563441b4fa2c108a
SupremusJohn <164743963+SupremusJohn@users.noreply.github.com>
2025-04-15 13:51:04 +0000

    Update teste.js

--- commandes/teste.js
+++ commandes/teste.js
@@ -4,37 +4,37 @@ const axios = require("axios");
 let intervalPing = null;
 let latenceTimeout = null;
 
-zokou({ nomCom: "pingweb", categorie: "MON-BOT", reaction: "⚡" }, async (origineMessage, zk, commandeOptions) => {
+zokou({ nomCom: "latence", categorie: "MON-BOT", reaction: "⚡" }, async (origineMessage, zk, commandeOptions) => {
     const { repondre } = commandeOptions;
 
     if (intervalPing) {
-        repondre("Le ping est déjà en cours...");
+        repondre("La latence est déjà en cours...");
         return;
     }
 
     intervalPing = setInterval(async () => {
         try {
-            const response = await axios.get("https://supremus-md-yn5h.onrender.com");
+            const response = await axios.get("https://zokouscan-din3.onrender.com");
             console.log(`[PING] ${new Date().toLocaleTimeString()} - Statut : ${response.status}`);
             await zk.sendMessage(origineMessage, { text: `Statut : ${response.status} (${new Date().toLocaleTimeString()})` });
         } catch (err) {
             console.error(`[PING] Erreur : ${err.message}`);
             await zk.sendMessage(origineMessage, { text: `Erreur : ${err.message}` });
         }
-    }, 10000);
+    }, 480000);
 
-    repondre("Ping démarré. Vérification toutes les 10 secondes.");
+    repondre("Latence démarré. Fin de la latence dans 8 minutes.");
 });
 
 
-zokou({ nomCom: "stopping", categorie: "MON-BOT", reaction: "🛑" }, async (origineMessage, zk, commandeOptions) => {
+zokou({ nomCom: "stop", categorie: "MON-BOT", reaction: "🛑" }, async (origineMessage, zk, commandeOptions) => {
     const { repondre } = commandeOptions;
 
     if (intervalPing) {
         clearInterval(intervalPing);
         intervalPing = null;
-        repondre("Ping arrêté.");
+        repondre("Latence arrêté.");
     } else {
-        repondre("Aucun ping en cours.");
+        repondre("Aucune latence en cours.");
     }
 });
