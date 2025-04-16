d0d5bfbe1dad22bc1c1b7fd1036169b191770268
SupremusJohn <164743963+SupremusJohn@users.noreply.github.com>
2025-04-16 16:59:36 +0000

    Create origamy_skins.js

---
+++ commandes/origamy_skins.js
@@ -0,0 +1,24 @@
+// Dictionnaire des engins avec leurs liens et catégorie
+
+const select_skins = {
+    'RANG_C': {
+        'COMMUNS': {
+            'KAIRO': { lien: 'https://i.ibb.co/hJqLQ8wF/Image-2025-04-16-15-52-59-3.jpg' },
+            'AËRON': { lien: 'https://i.ibb.co/gFDxCXDn/Image-2025-04-16-15-06-34-6.jpg' },
+            'RYU': { lien: 'https://i.ibb.co/7J0rNzVq/Image-2025-04-16-15-06-34-5.jpg' },
+            'HYDRON': { lien: 'https://i.ibb.co/dFm3xXP/Image-2025-04-16-15-06-34-7.jpg' },
+            'FLORA': { lien: 'https://i.ibb.co/LX98wmnv/Image-2025-04-16-15-52-59-0.jpg' }
+        },
+        'RARES': {
+            'NATSUKI': { lien: 'https://i.ibb.co/Ld4BxYPR/Image-2025-04-16-15-06-34-9.jpg' },
+            'LUNA': { lien: 'https://i.ibb.co/5hp76YQd/Image-2025-04-16-15-52-59-4.jpg' },
+            'THORNE': { lien: 'https://i.ibb.co/JDCZtmB/Image-2025-04-16-15-52-59-2.jpg' },
+            'FULGURA': { lien: 'https://i.ibb.co/BVkbSpGg/Image-2025-04-16-15-52-59-1.jpg' },
+            'AELIS': { lien: 'https://i.ibb.co/MxPzCxYP/Image-2025-04-16-15-06-34-2.jpg' }
+        }
+    }
+};
+
+module.exports = {
+    select_skins
+};
