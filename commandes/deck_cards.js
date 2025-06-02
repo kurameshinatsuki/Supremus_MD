d4a4856970176f29b422f8f2b18a07cee3bf6fe4
kurameshi natsuki <111725926+kurameshinatsuki@users.noreply.github.com>
2025-06-02 21:05:41 +0000

    Create deck_cards.js

---
+++ commandes/deck_cards.js
@@ -0,0 +1,77 @@
+// 📁 commande/deck_cards.js
+
+const deck_cards = {
+  // PEGASUS
+  'Défense Toon': 'https://i.ibb.co/JjYT02ck/LDS1-70.jpg',
+  'Table Des Matières Toon': 'https://i.ibb.co/ZR7HYL33/LDS1-69P.jpg',
+  'Crâne Invoqué Toon': 'https://i.ibb.co/VY47WV0Y/LDS1-55.jpg',
+  'Monde Des Toons': 'https://i.ibb.co/PZWTMV5Z/LDS1-68P.jpg',
+  'Sirène Toon': 'https://i.ibb.co/BVWFwK3M/LDS1-54.jpg',
+  'Gearfried le Chevalier de Fer': 'https://i.ibb.co/LD5GxPkW/Gearfriedle-Chevalierde-Fer-SS02-FR-C-1-E.webp',
+  'Pommes d\'Or': 'https://i.ibb.co/BKwp6q8g/EGS1-37.jpg',
+  'Doppelganger le Mimétique': 'https://i.ibb.co/ymprPydt/DLCS-78P.jpg',
+  'Rembobinage Toon': 'https://i.ibb.co/cKKYwhgx/DLCS-75.jpg',
+  'Dans le Monde des Toons': 'https://i.ibb.co/yc9yM94j/Dansle-Mondedes-Toons-SS01-FR-C-1-E.webp',
+
+  // KAIBA
+  'Esprit de Combat': 'https://i.ibb.co/tMBjvdvJ/STAS-77.jpg',
+  'Renforts': 'https://i.ibb.co/JRR93vKx/SS03-26.jpg',
+  'Raclée': 'https://i.ibb.co/kVJCBfCB/SS02-19.jpg',
+  'Esprit Ryu': 'https://i.ibb.co/84sg3p49/SS02-8.jpg',
+  'Dragon Tyran': 'https://i.ibb.co/QF9HGntt/SS02-7.jpg',
+  'Boeuf de Combat': 'https://i.ibb.co/cKrjQNFM/SS02-2.jpg',
+  'Invocateur Dragon Bleu': 'https://i.ibb.co/rK31JR4d/SS01-8.jpg',
+  'Encrassement Destructeur': 'https://i.ibb.co/NgQBL687/SGX3-B13.jpg',
+  'Dragon Étincelant N°2': 'https://i.ibb.co/212YRqHh/SGX3-B04.jpg',
+  'Bouclier et Épée': 'https://i.ibb.co/pvwKNpRf/SBLS-14.jpg',
+  'Michizure': 'https://i.ibb.co/hJW3P2Q3/Michizure-SBLS-FR-UR-1-E.webp',
+  'Rayon Nocturne': 'https://i.ibb.co/CTL08qT/Rayon-Nocturne-SDPL-FR-C-1-E.webp',
+  'Kaibaman': 'https://i.ibb.co/S7RvSS4c/LDS2-2.jpg',
+  'Flot Rugissant de Destruction': 'https://i.ibb.co/ch3v6GsL/Flot-Rugissantde-Destruction-SS02-FR-C-1-E.webp',
+  'Dragon Blanc aux Yeux Bleus': 'https://i.ibb.co/MywJMc2y/Dragon-Blancaux-Yeux-Bleus-SS02-FR-C-1-E.webp',
+  'Dé-crâne': 'https://i.ibb.co/NdK6byvz/D3-Fne-SS02-FR-C-1-E.webp',
+  'Béhémoth à Deux Têtes': 'https://i.ibb.co/p6jJKzLN/B3-Fmoth3-Ftes-SS02-FR-C-1-E.webp',
+
+  // JOEY
+  'Orbe de Sécurité': 'https://i.ibb.co/Z1HBstvH/SS04-59.jpg',
+  'Cercle Envoûtant': 'https://i.ibb.co/MyccXrfY/SS04-28.jpg',
+  'Pote à Moi Risation': 'https://i.ibb.co/Q3g225L7/SS02-45.jpg',
+  'Dragon Météore': 'https://i.ibb.co/9mtQHkrc/SS02-22.jpg',
+  'Esprit des Yeux Rouges': 'https://i.ibb.co/wNWT8bxX/SGX3-B19.jpg',
+  'Tempête d\'Etaqua': 'https://i.ibb.co/JwknNt6H/SGX3-A20.jpg',
+  'Dragon Millénaire': 'https://i.ibb.co/ymYHMjch/Dragon-Mill-naire-SS02-FR-C-1-E.webp',
+  'Chevalier Commandeur': 'https://i.ibb.co/XkbFCXyC/Chevalier-Commandeur-LDK2-FR-C-1-E.webp',
+  'Dephon': 'https://i.ibb.co/YBRmWWSN/D-phon-SS02-FR-C-1-E.webp',
+  'Kunai avec Chaîne': 'https://i.ibb.co/tTHm7DrY/RA03-204q.jpg',
+  'Axe Raider': 'https://i.ibb.co/4RR2CzX1/SBAD-9.jpg',
+  'Polymérisation': 'https://i.ibb.co/nq9nLXX5/RA03-51-Bc.jpg',
+  'Dragon Noir aux Yeux Bleus': 'https://i.ibb.co/sv2SrvjR/Dragon-Noiraux-Yeux-Rouges-RA04-FR-QCSE-1-E.webp',
+  'Dragon Noir Météore': 'https://i.ibb.co/DfWyj6Sm/LDS1-13.jpg',
+
+  // MAI
+  'Tornade Violente': 'https://i.ibb.co/60dyry5D/47766694.jpg',
+  'Terrains de Chasse des Harpies': 'https://i.ibb.co/pj0BdG8S/LDS2-81.jpg',
+  'Égotiste Élégant': 'https://i.ibb.co/FbfgWhxm/LDS2-80.jpg',
+  'À Moitié Fermé': 'https://i.ibb.co/nqgwNf34/73048641.jpg',
+  'Visage d\'Oiseau': 'https://i.ibb.co/mVcKsbsh/SS02-53.jpg',
+  'Dame Harpie 1': 'https://i.ibb.co/5XYxFx0r/HAC1-10p.jpg',
+  'Amazonesse avec Chaîne': 'https://i.ibb.co/6RXrJsg9/SGX3-D05.jpg',
+  'Synergie Tribale': 'https://i.ibb.co/21mDk0PR/SS02-66.jpg',
+  'Sœur de Harpie': 'https://i.ibb.co/CKtx2RW6/LDS2-65-1.jpg',
+
+  // YAMI
+  'Quinte Flush Royale': 'https://i.ibb.co/DD5BX7LV/SBLS-49.jpg',
+  'Alakan le Chevalier Harlequin': 'https://i.ibb.co/VYKzkjcg/KICO-29-1.jpg',
+  'Conscription': 'https://i.ibb.co/d04k37Gk/SBLS-24.jpg',
+  'Sogen': 'https://i.ibb.co/GfVbrq0m/SS02-34.jpg',
+  'Épée de l\'Ame Draconique': 'https://i.ibb.co/Cp4yB1rP/61405855.jpg',
+  'Sphère Kuriboh': 'https://i.ibb.co/MyQ5skbv/SBC1-114.jpg',
+  'Épée Amazonesse': 'https://i.ibb.co/wFx6DWzB/SGX3-D02-S.jpg',
+  'Chevalier du Roi': 'https://i.ibb.co/4RfKXrCv/64788463.jpg',
+  'Chevalier de le Reine': 'https://i.ibb.co/nNGKpYKF/STAS-45.jpg',
+  'Chevalier du Valet': 'https://i.ibb.co/5XQ69YF4/90876561.jpg',
+};
+
+module.exports = {
+  deck_cards
+};
