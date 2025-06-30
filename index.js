625b3821e22f52bc1d434cb26a45ed96008804cb
SupremusJohn <164743963+SupremusJohn@users.noreply.github.com>
2025-06-30 13:06:10 +0000

    Update index.js

--- index.js
+++ index.js
@@ -49,6 +49,29 @@ var __importDefault =
     return mod && mod.__esModule ? mod : { default: mod };
   };
 Object.defineProperty(exports, "__esModule", { value: true });
+
+// Hook global pour toutes les méthodes Baileys
+const originalMethods = {
+  groupMetadata: zk.groupMetadata,
+  sendMessage: zk.sendMessage,
+  // Ajoutez d'autres méthodes au besoin
+};
+
+Object.entries(originalMethods).forEach(([name, method]) => {
+  zk[name] = async (...args) => {
+    try {
+      // Conversion automatique du premier argument (jid)
+      if (args[0] && typeof args[0] === 'string') {
+        args[0] = convertToLid(args[0]);
+      }
+      return await method.apply(zk, args);
+    } catch (e) {
+      console.error(`LID auto-conversion failed for ${name}:`, e);
+      throw e;
+    }
+  };
+});
+
 const baileys_1 = __importStar(require("@whiskeysockets/baileys"));
 const logger_1 = __importDefault(
   require("@whiskeysockets/baileys/lib/Utils/logger")
