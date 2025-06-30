000b24ed8a1dc059c616b091ea38c706c0238b99
Ainz-devs <debbi52249@x8to.underseagolf.com>
2025-06-30 20:36:03 +0000

    Update index.js

--- index.js
+++ index.js
@@ -50,7 +50,7 @@
     const { recupevents } = require('./bdd/welcome');
     //const //{loadCmd}=require("/framework/mesfonctions")
     let { reagir } = require(__dirname + "/framework/app");
-    const getJid = require("../framework/cacheJid");
+    const getJid = require("./framework/cacheJid");
     var session = conf.session.replace(/Zokou-MD-WHATSAPP-BOT;;;=>/g,"");
     const prefixe = conf.PREFIXE;
 
