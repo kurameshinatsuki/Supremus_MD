bf1264ba5220ca2fe975e596e55978aa4e500a77
kurameshi natsuki <111725926+kurameshinatsuki@users.noreply.github.com>
2025-08-19 17:56:51 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1067,7 +1067,7 @@ app.get('/', (req, res) => {
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Thanks for using Zokou</title>
+        <title>Supremus-MD by John Supremus</title>
         <style>
             /* Styles pour centrer le texte */
             body {
