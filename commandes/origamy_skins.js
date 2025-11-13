3ccde51ae8c11302a04128a5f714be147f7f215c
kurameshi natsuki <111725926+kurameshinatsuki@users.noreply.github.com>
2025-11-13 21:58:10 +0000

    Update origamy_skins.js

--- commandes/origamy_skins.js
+++ commandes/origamy_skins.js
@@ -3,18 +3,14 @@
 const select_skins = {
     'RANG_C': {
         'COMMUNS': {
-            'KAIRO': { lien: 'https://i.ibb.co/hJqLQ8wF/Image-2025-04-16-15-52-59-3.jpg' },
-            'AËRON': { lien: 'https://i.ibb.co/gFDxCXDn/Image-2025-04-16-15-06-34-6.jpg' },
-            'RYU': { lien: 'https://i.ibb.co/7J0rNzVq/Image-2025-04-16-15-06-34-5.jpg' },
-            'HYDRON': { lien: 'https://i.ibb.co/dFm3xXP/Image-2025-04-16-15-06-34-7.jpg' },
-            'FLORA': { lien: 'https://i.ibb.co/LX98wmnv/Image-2025-04-16-15-52-59-0.jpg' }
+            'FLORA': { lien: 'https://i.ibb.co/Zp7vSvss/flora-mononature-ow-skins.jpg' },
+            'ELENA': { lien: 'https://i.ibb.co/TVPCSBv/elena-mononature-ow-skins.jpg' },
+            'EIDEN': { lien: 'https://i.ibb.co/TBGsP15f/eiden-mononature-ow-skins.jpg' }
         },
         'RARES': {
-            'NATSUKI': { lien: 'https://i.ibb.co/Ld4BxYPR/Image-2025-04-16-15-06-34-9.jpg' },
-            'LUNA': { lien: 'https://i.ibb.co/5hp76YQd/Image-2025-04-16-15-52-59-4.jpg' },
-            'THORNE': { lien: 'https://i.ibb.co/JDCZtmB/Image-2025-04-16-15-52-59-2.jpg' },
-            'FULGURA': { lien: 'https://i.ibb.co/BVkbSpGg/Image-2025-04-16-15-52-59-1.jpg' },
-            'AELIS': { lien: 'https://i.ibb.co/MxPzCxYP/Image-2025-04-16-15-06-34-2.jpg' }
+            'NATSUKI': { lien: 'https://i.ibb.co/rRkg0hTd/natsuki-mononature-ow-skins.jpg' },
+            'KAIRO': { lien: 'https://i.ibb.co/7JMkk3Pz/kairo-arcaniste-ow-skins.jpg' },
+            'THORNE': { lien: 'https://i.ibb.co/R49ZbM6C/thorne-mononature-ow-skins.jpg' }
         }
     }
 };
